2bd25e363bfccea1dd30e6bff44219d46c153520
Khalid Saifullah <khalid@outlook.com.au>
2017-04-21 20:32:19 +1000

    Clean up

--- vagrant/scripts/install-docker.sh
+++ vagrant/scripts/install-docker.sh
@@ -17,25 +17,19 @@ fi
 apt-get update > /dev/null
 
 # Install packages to allow apt to use a repository over HTTPS:
-echo "********************** Install default packages **********************"
 apt-get install apt-transport-https --assume-yes
 apt-get install ca-certificates --assume-yes
 apt-get install curl --assume-yes
 apt-get install software-properties-common --assume-yes
-echo "********************** End of installation **********************"
 
 # Add Docker’s official GPG key:
-echo "********************** Get docker GPG key **********************"
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo "********************** Docker GPG key downloaded **********************"
 
 # Set up the stable docker repository.
-echo "********************** Set up docker repo **********************"
 add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
-echo "********************** Docker repo is set **********************"
 
 # Update package information.
 apt-get update > /dev/null
@@ -43,4 +37,4 @@ apt-get update > /dev/null
 # Install the latest version of Docker
 echo "********************** Install Docker **********************"
 apt-get install docker-ce --assume-yes
-echo "********************** Finish docker installation **********************"
+echo "********************** Done **********************"

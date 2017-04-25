6c7c82922d06e424b9b4a0b3ce5f2ee4da28a3cb
Khalid Saifullah <khalid@outlook.com.au>
2017-04-25 17:18:45 +1000

    Custom mysql container.

---
+++ apps/mysql/init.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Local of sql dump files.
+DUMP="/tmp/dump"
+
+# Exit it the dump directory doesn't exists.
+if [ ! -d ${DUMP} ]; then
+  exit 0
+fi
+
+# MySQL account credentials.
+USER="root"
+PASS="admin"
+
+# Stop the execution of a script if a command or pipeline has an error.
+set -e
+
+# Loop through all the dump files and create user and db with same name.
+for filename in $(ls -a ${DUMP} | grep .sql); do
+  # Get the first part before any dot (.) comes.
+  name=$(echo $filename | cut -d'.' -f1)
+
+  # Get only alpha-numeric characters.
+  dbname=$(echo $name | sed 's/[^a-zA-Z0-9]//g')
+
+  # Create database, user account and grant privileges.
+  mysql -u${USER} -p${PASS} -e "
+    DROP DATABASE IF EXISTS ${dbname};
+    CREATE DATABASE IF NOT EXISTS ${dbname};
+    CREATE USER '${dbname}'@'%' IDENTIFIED BY '${dbname}';
+    GRANT ALL PRIVILEGES ON ${dbname}.* TO '${dbname}'@'%';
+    FLUSH PRIVILEGES;"
+  
+  # Unzip db dump file if already zipped and import in the db.
+  if [ "${filename##*.}" != "sql" ]; then
+    zcat ${DUMP}/${filename} | mysql -u${USER} -p${PASS} $dbname
+  else
+    mysql -u${USER} -p${PASS} $dbname < ${DUMP}/${filename}
+  fi
+done
